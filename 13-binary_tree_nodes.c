3a44b2b84e4099cf78b79586afbb6da7b99aa51b
samuel212121 <abemmenelik@gmail.com>
2024-06-06 08:28:56 -0700

    count node atleat one

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,15 +1,12 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_leaves - counts leaf nodes
+  * binary_tree_nodes - counts nodes with at least 1 child
   * @tree: tree to measure
-  * Return: number of leaf nodes
+  * Return: number of nodes with children
   */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
-
+	return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
 }
